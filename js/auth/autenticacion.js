00ca1a73114b6e6a3a10a5bb7c30e0b2c684358c
AndresCalvoG <andrescalvo9407@gmail.com>
2021-05-03 18:40:41 -0500

    Validacion de campos vacios

--- js/auth/autenticacion.js
+++ js/auth/autenticacion.js
@@ -18,17 +18,31 @@ class Autenticacion{
                 handleCodeInApp: true,
             }
             //result.user.sendEmailVerification(configuracion)
-            firebase.auth().sendSignInLinkToEmail(email, configuracion)
-            .then(() =>{ 
-                console.log('mensaje enviado')
-            })
-            .catch(error =>{
-                console.error(error)
-               // materialize.toast(error.message, 4000)
-            })
+            // firebase.auth().sendSignInLinkToEmail(email, configuracion)
+            // .then(() =>{ 
+            //     console.log('mensaje enviado') 
+            // })
+            // .catch(error =>{
+            //     console.error(error)
+            //    // materialize.toast(error.message, 4000)
+            // })
 
             firebase.auth().signOut()
            // materialize.toast(`Bienvenido ${nombres}, debe realizar el proceso de verificacion`, 4000)
+            let container = document.getElementById('contain');
+            let body = document.getElementById('body');
+            container.innerHTML="";
+            container.style.justifyContent = "center"
+            container.style.textAlign = "center"
+            body.style.color = "#999999"
+            container.innerHTML = `
+                  <h1> Bienvenido </h1>
+                  <br>
+                  <br>
+                  <p>${nombres}</p>
+                  <p> Debes realizar el proceso de verificacion desde el correo enviado a tu email </p>
+             `;
+
            console.log(`Bienvenido ${nombres}, debe realizar el proceso de verificacion`)
             //$('.modal').modal('close')
         })
--- js/auth/controllerAuth.js
+++ js/auth/controllerAuth.js
@@ -7,7 +7,12 @@ enviar.onclick = () => {
     const nombres = `${nombre.value} ${apellido.value}`
     const email = document.getElementById('correo')
     const password = document.getElementById('password')
+    const code = document.getElementById('code')
     
+    if(nombre.value === "" || apellido.value === "" || email.value === "" || password.value === "" || code.value === ""){
+        alert('Por favor completa TODOS los campos')
+    }else{ 
     const auth = new Autenticacion()
     auth.crearCuentaEmailPass(email.value,password.value,nombres)
+     }
 };
