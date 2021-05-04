4ccadf7103556079fda9268af1f426bbad355154
AndresCalvoG <andrescalvo9407@gmail.com>
2021-05-04 08:07:40 -0500

    incluido metodo de logout

---
+++ js/auth/loggin.js
@@ -0,0 +1,16 @@
+const ingresar = document.getElementById('loggin')
+
+ingresar.onclick = ()=> {
+    let email = document.getElementById('user')
+    email = email.value.trim().toUpperCase()
+    let password = document.getElementById('pass')
+    password = password.value.trim().toUpperCase()
+    
+    if( email === "" || password === "" ){
+        alert('Por favor completa TODOS los campos')
+    }else{ 
+        console.log(email, password)
+    const auth = new Autenticacion()
+    auth.autEmailPass(email, password)
+     }
+};
---
+++ js/auth/logout.js
@@ -0,0 +1,14 @@
+const salir = document.getElementById('logout')
+
+salir.onclick = ()=> {
+
+    firebase.auth().signOut()
+    .then(() => {
+        console.log('salida correcta')
+        window.location.replace("../../index.html")
+    })
+    .catch(error =>{
+        console.log('no ha salido')
+    })
+
+};
