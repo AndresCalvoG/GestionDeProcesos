5354ff03ca00a8d4744fb0e70b8a209a157182c8
AndresCalvoG <andrescalvo9407@gmail.com>
2021-07-31 13:03:48 -0500

    cambio de ruta salida1

--- js/auth/logout.js
+++ js/auth/logout.js
@@ -5,7 +5,7 @@ salir.onclick = ()=> {
     firebase.auth().signOut()
     .then(() => {
         console.log('salida correcta')
-        window.location.replace("../..")
+        window.location.replace("../GestionDeProcesos")
         // window.location.replace("https://andrescalvog.github.io/GestionDeProcesos/")
     })
     .catch(error =>{
