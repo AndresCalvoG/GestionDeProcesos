05f65c12f81f1b2f10229c4d205c2bf4a6e883ff
Andres Calvo <AndresCalvo9407@gmail.com>
2021-11-04 20:18:21 -0500

    profile page

--- gestioner/src/pages/Profile.js
+++ gestioner/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ImageUser from "../components/ImageUser";
 import Modal from "../components/Modal";
 import Button from "../components/Button";
@@ -7,8 +7,13 @@ import { AppContext } from "../context";
 import "./styles/profile.css";
 
 function Profile() {
-  const { photoUrl, setPhotoUrl } = React.useContext(AppContext);
+  const { photoUrl, setPhotoUrl, user } = React.useContext(AppContext);
   const [photo, setPhoto] = useState("");
+
+  useEffect(() => {
+    var menu = document.getElementById("modalMenu");
+    menu.classList.replace("modalBackground", "hidenModal");
+  }, []);
 
   function changeImageUser() {
     setPhoto("");
@@ -24,6 +29,18 @@ function Profile() {
     <main className="main-container">
       <section className="profileCard">
         <ImageUser action={changeImageUser} />
+        <div className="profile-info">
+          <p>Nombre:</p>
+          <p>
+            {user.fields.first.stringValue} {user.fields.last.stringValue}
+          </p>
+          <p>Email:</p>
+          <p>{user.fields.email.stringValue}</p>
+          <p>Cargo:</p>
+          <p>{user.fields.cargo.stringValue}</p>
+          <p>Codigo:</p>
+          <p>{user.fields.code.stringValue}</p>
+        </div>
       </section>
       <Modal id="modalProfile">
         <div className="main-modal">
