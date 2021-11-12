d7d48ea3ca5eb95a0f9c779a3bc4518c41f7b82a
AndresCalvoG <andrescalvo9407@gmail.com>
2021-11-12 11:07:04 -0500

    terminado maquetado y estilos

--- src/components/Viwer.js
+++ src/components/Viwer.js
@@ -6,21 +6,30 @@ function Viwer() {
     <section className="viwer-container">
       <h1 className="viwer-title">Contraseñas</h1>
       <article className="viwer-info">
-        <h3 className="info-title">Envase</h3>
+        <div className="info-title">
+          <h3 className="info-title--area">Envase:</h3>
+          <h3 className="info-title--machine">Blister 5</h3>
+        </div>
         <div className="info-card">
-          <h3>Blister 5</h3>
           <table className="info-table">
-            <tr className="table-row">
-              <td>nombre</td>
-              <td>usuario</td>
-              <td>contraseña</td>
-              <td>tipo</td>
+            <tr>
+              <td colSpan="4">
+                <u>
+                  <i>Camara Visio Chrome:</i>
+                </u>
+              </td>
             </tr>
-            <tr className="table-row">
-              <td>Andres Calvo</td>
-              <td>19939</td>
-              <td>12345</td>
-              <td>admin</td>
+            <tr>
+              <th className="row-celd--title">Nombre</th>
+              <th className="row-celd--title">Usuario</th>
+              <th className="row-celd--title">Contraseña</th>
+              <th className="row-celd--title">Tipo</th>
+            </tr>
+            <tr>
+              <td className="row-celd--item">Andres Calvo</td>
+              <td className="row-celd--item">19939</td>
+              <td className="row-celd--item">12345</td>
+              <td className="row-celd--item">admin</td>
             </tr>
           </table>
         </div>
