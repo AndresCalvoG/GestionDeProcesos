8feb45cf06eb81d39acf3ab3f4649aec0c492552
AndresCalvoG <andrescalvo9407@gmail.com>
2021-11-20 13:32:30 -0500

    menu dispositivos

--- src/pages/Machines.js
+++ src/pages/Machines.js
@@ -11,6 +11,7 @@ function Machines() {
   const [clase, setClase] = useState("hidenModal");
   const [classe, setClasse] = useState("hidenModal");
   const [claseData, setClaseData] = useState("hidenModal");
+  const [claseSelect, setClaseSelect] = useState("hidenModal");
   const [photoName, setPhotoName] = useState("");
   const [photo, setPhoto] = useState("");
   const [file, setFile] = useState("");
@@ -19,6 +20,10 @@ function Machines() {
   const [refer, setRefer] = useState("");
   const [area, setArea] = useState("");
   const [cubiculo, setCubiculo] = useState("");
+  const [display, setDisplay] = useState([false, false]);
+  const [marca, setMarca] = useState("");
+  const [camera, setCamera] = useState([false, false]);
+  const [marcam, setMarcam] = useState("");
 
   const showModalDel = () => {
     if (clase === "hidenModal") {
@@ -39,6 +44,10 @@ function Machines() {
   function showModalData() {
     setClasse("hidenModal");
     setClaseData("showModal-full");
+  }
+  function showModalSelect() {
+    setClaseData("hidenModal");
+    setClaseSelect("showModal-full");
   }
 
   return (
@@ -126,9 +135,117 @@ function Machines() {
               class="inputFormOrder"
             />
           </label>
+          <br />
           <div className="modalKeypad">
-            <Button name="Siguiente" class="modalMenu" action={showModalData} />
-            <Button name="Atras" class="modalMenu" action={showModalAdd} />
+            <Button
+              name="Siguiente"
+              class="modalMenu"
+              action={showModalSelect}
+            />
+            <Button
+              name="Atras"
+              class="modalMenu"
+              action={() => {
+                setClasse("showModal-full");
+                setClaseData("hidenModal");
+              }}
+            />
+          </div>
+        </div>
+      </Modal>
+      <Modal classe={claseSelect}>
+        <div className="main-modal">
+          <h4>Selecciona los componentes </h4>
+          <br />
+          <div className="modal-select">
+            <p>¿Tiene Pantalla HMI?</p>
+            <label>
+              Si
+              <InputForm
+                type="checkbox"
+                value={display[0]}
+                action={setDisplay}
+                index={0}
+                array={display.length}
+              />
+            </label>
+            <label>
+              No
+              <InputForm
+                type="checkbox"
+                value={display[1]}
+                action={setDisplay}
+                index={1}
+                array={display.length}
+              />
+            </label>
+          </div>
+          <br />
+          {display[0] ? (
+            <label>
+              Marca:
+              <InputForm
+                type="text"
+                size="20"
+                value={marca}
+                action={setMarca}
+                readOnly={false}
+                class="inputFormOrder"
+              />
+            </label>
+          ) : (
+            <p></p>
+          )}
+          <br />
+          <div className="modal-select">
+            <p>¿Tiene Camara?</p>
+            <label>
+              Si
+              <InputForm
+                type="checkbox"
+                value={camera[0]}
+                action={setCamera}
+                index={0}
+                array={camera.length}
+              />
+            </label>
+            <label>
+              No
+              <InputForm
+                type="checkbox"
+                value={camera[1]}
+                action={setCamera}
+                index={1}
+                array={camera.length}
+              />
+            </label>
+          </div>
+          <br />
+          {camera[0] ? (
+            <label>
+              Marca:
+              <InputForm
+                type="text"
+                size="20"
+                value={marcam}
+                action={setMarcam}
+                readOnly={false}
+                class="inputFormOrder"
+              />
+            </label>
+          ) : (
+            <p></p>
+          )}
+          <div className="modalKeypad">
+            <Button name="siguiente" class="modalMenu" action={showModalData} />
+            <Button
+              name="atras"
+              class="modalMenu"
+              action={() => {
+                setClaseData("showModal-full");
+                setClaseSelect("hidenModal");
+              }}
+            />
           </div>
         </div>
       </Modal>
