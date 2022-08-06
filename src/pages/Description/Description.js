388403beb28005e6867c0a75ca28be78f7aeeb37
Andres Calvo <andrescalvo9407@gmail.com>
2022-08-05 22:25:03 -0500

    create table description

--- src/pages/Description/Description.js
+++ src/pages/Description/Description.js
@@ -7,8 +7,8 @@ const Container = Styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: var(--blue);
-  color: var(--white);
+  background-color: var(--white);
+  color: var(--blue);
 
   figure{
     width: 90%;
@@ -21,6 +21,7 @@ const Container = Styled.section`
     width: 90%;
     h1{
       font-size: 4rem;
+      margin: 2rem 0 2rem 0;
     }
     p{
       font-size: 2rem;
@@ -31,23 +32,26 @@ const Tabs = Styled.div`
   width: 90%;
   overflow: hidden;
   border: 1px solid #ccc;
-  background-color: var(--white);
+  background-color: var(--blue);
+  margin-top: 2rem; 
 
   button{
     background-color: inherit;
-  float: left;
-  border: none;
-  outline: none;
-  cursor: pointer;
-  padding: 14px 16px;
-  transition: 0.3s;
+    float: left;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 14px 16px;
+    transition: 0.3s;
+    color: var(--white);
+    font-size: 1.6rem;
 
-  &:hover{
-    background-color: #ddd;
-  }
-  &.active{
-    background-color: #ccc;
-  }
+    &:hover{
+      background-color: var(--purple);
+    }
+    &.active{
+      background-color: #ccc;
+    }
   }
 `;
 
@@ -55,6 +59,22 @@ const TabContent = Styled.article`
   padding: 6px 12px;
   border: 1px solid #ccc;
   border-top: none;
+  
+  table{
+    width: 100%;
+    height: 20rem;
+    font-size: 2rem;
+    border-collapse: collapse;
+    td{
+      border-bottom: 1px solid var(--blue);
+      padding: 0.5rem;
+    }
+    tr:hover{
+      background-color: var(--purple);
+      color: var(--white);
+    }
+    
+  }
 `;
 
 function Description() {
@@ -89,7 +109,7 @@ function Description() {
             setCurrentTab(0);
           }}
         >
-          Sellado
+          Especificaciones
         </button>
         <button
           className="tablinks"
@@ -97,7 +117,7 @@ function Description() {
             setCurrentTab(1);
           }}
         >
-          Formado
+          Termoformado
         </button>
         <button
           className="tablinks"
@@ -110,13 +130,92 @@ function Description() {
       </Tabs>
       {currentTab === 0 ? (
         <TabContent>
-          <h3>London</h3>
-          <p>London is the capital city of England.</p>
+          <table>
+            <tr>
+              <td>Longitud de moldeo</td>
+              <td>130mm</td>
+            </tr>
+            <tr>
+              <td>Ancho moldura</td>
+              <td>85mm</td>
+            </tr>
+            <tr>
+              <td>profundidad de formación</td>
+              <td>10mm o 18.5mm</td>
+            </tr>
+            <tr>
+              <td>Ancho máximo de lámina</td>
+              <td>168mm max.</td>
+            </tr>
+            <tr>
+              <td>Diametro bobinas</td>
+              <td>pvc: max. 400mm aluminio: max. 240mm</td>
+            </tr>
+            <tr>
+              <td>Ciclo/min</td>
+              <td>P. formado: 10mm / 50 -- 18.5mm / 35</td>
+            </tr>
+            <tr>
+              <td>Consumo nominal de corriente</td>
+              <td>8 KW</td>
+            </tr>
+            <tr>
+              <td>Potencia media absorbida</td>
+              <td>5 KW</td>
+            </tr>
+            <tr>
+              <td>Consumo de agua refrigeradora</td>
+              <td>
+                0.2 m<sup>3</sup>/h
+              </td>
+            </tr>
+            <tr>
+              <td>Consumo de aire comprimido</td>
+              <td>
+                15 m<sup>3</sup>/h max.
+              </td>
+            </tr>
+            <tr>
+              <td>Presion de entrada</td>
+              <td>6 ... 8 bar</td>
+            </tr>
+            <tr>
+              <td>Largo X Ancho x Altura (mm)</td>
+              <td>3600 x 955 x 1470</td>
+            </tr>
+            <tr>
+              <td>Peso</td>
+              <td>1400 Kg aprox.</td>
+            </tr>
+          </table>
         </TabContent>
       ) : currentTab === 1 ? (
         <TabContent>
-          <h3>Paris</h3>
-          <p>Paris is the capital of France.</p>
+          <p>
+            El termoformado es un proceso de gran rendimiento para la
+            realización de productos de plástico a partir de láminas
+            semielaboradas, que hallan numerosos campos de aplicación, desde el
+            envase a piezas para electrodomésticos y automoción
+          </p>
+          <h2>Métodos de conformado</h2>
+          <p>
+            El sistema más simple es el estirado de una lámina en estado
+            semi-plástico sobre un molde. A medida que la lámina topa con la
+            superficie del molde, el estirado se detiene y, como resultado, las
+            partes de la lámina que tocan al molde en primer lugar tienen un
+            espesor mayor que el resto. Si el estirado es pequeño, no queda
+            comprometida la integridad de la pieza y, por tanto, es el
+            procedimiento más usado en el envase de tipo "blíster" y en los
+            embalajes de tipo burbuja.
+          </p>
+          <p>
+            Formado a presión: similar al moldeo por vacío, sobre la lámina se
+            aplica además aire comprimido hasta 1,4 MPa, por lo que el sistema
+            precisa de una cámara cerrada superior. Este procedimiento se
+            utiliza para conformar lámina de pequeña galga de materiales como el
+            PP, que se suministran en rollo, o para transformar lámina de gran
+            espesor en piezas con detalle superficial fino.
+          </p>
         </TabContent>
       ) : currentTab === 2 ? (
         <TabContent>
