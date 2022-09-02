70333141a5e6a953082a8924c4ff74c340301106
Andres Calvo <andrescalvo9407@gmail.com>
2022-09-01 22:21:57 -0500

    structuring content extra hours form

--- src/pages/ExtraHours/ExtraHours.js
+++ src/pages/ExtraHours/ExtraHours.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Style from "styled-components";
 
+import Input from "../../components/InputForm";
+import Button from "../../components/Buttons/Button";
+
 function ExtraHours() {
   return (
-    <>
-      <h1>Horas Extras</h1>
-      <p>Ingresa tus horas:</p>
-    </>
+    <main>
+      <h1>Horas Extra</h1>
+      <section>
+        <article>
+          <h3>Nueva horario extra</h3>
+          <Input type="text" label="Nombre completo" />
+          <Input type="text" label="Salario menseual" />
+          <Input type="date" label="fecha" />
+          <Input type="time" label="Hora de inicio" />
+          <Input type="time" label="Hora de fin" />
+          <Button name="Calcular" type="basic" invertColor={true} />
+        </article>
+      </section>
+    </main>
   );
 }
 
