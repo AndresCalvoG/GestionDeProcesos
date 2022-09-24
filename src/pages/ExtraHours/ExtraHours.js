dcc792bbc66981f3bd58c5af6a52faca55a5a078
Andres Calvo <andrescalvo9407@gmail.com>
2022-09-24 05:14:10 -0500

    new code control

--- src/pages/ExtraHours/ExtraHours.js
+++ src/pages/ExtraHours/ExtraHours.js
@@ -57,85 +57,148 @@ function ExtraHours() {
   const [startDate, setStartDate] = useState(timeObj);
   const [endDate, setEndDate] = useState(timeObj);
   const [holyStart, setHolyStart] = useState(false);
-  const [fiveDays, setFiveDays]= useState(false)
-  const [sixDays, setSixDays]= useState(false)
+  const [fiveDays, setFiveDays] = useState(false);
+  const [sixDays, setSixDays] = useState(false);
   const [holyEnd, setHolyEnd] = useState(false);
   const [results, setResults] = useState({});
 
-  function calcExtraHour() {
+  function validation() {
     if (salary === 0 || salary < 1000000) {
       console.log("debe ingresaria mas de 1 millon cop");
-    }
-    if (startDate.millis === 0 || endDate.millis === 0) {
+      return false;
+    } else if (!fiveDays && !sixDays) {
+      console.log("seleccione una jornada");
+      return false;
+    } else if (startDate.millis === 0 || endDate.millis === 0) {
       console.log("complete all spaces");
+      return false;
+    } else {
+      return true;
+    }
+  }
+  function calcExtraHour() {
+    if (validation()) {
+      let hoursMillis = endDate.millis - startDate.millis;
+      let totalHours = (hoursMillis / 3600000).toFixed(2);
+      let ordinaryHours = 0;
+      let hed = 0;
+      let nigthHours = 0;
+      let hen = 0;
+      let dominicalNigthHours = 0;
+      let dominicalOrdinaryHours = 0;
+      let holyOrdinaryHours = 0;
+      let holyNigthHours = 0;
+
+      let numCycles = hoursMillis / 60000;
+
+      let startDay = new Date(startDate.millis).getDay();
+
+      for (let i = 0; i < numCycles; i++) {
+        let hour = new Date(startDate.millis + 60000 * i).getHours();
+        let day = new Date(startDate.millis + 60000 * i).getDay();
+
+        if (sixDays) {
+          if (day === 0) {
+            // domingos
+            if (i >= 490) {
+              //horas extra
+              if (hour >= 6 && hour < 21) {
+              }
+              if ((hour >= 0 && hour < 6) || (hour >= 21 && hour < 24)) {
+              }
+            } else {
+              //horas ordinarias
+              if (hour >= 6 && hour < 21) {
+              }
+              if ((hour >= 0 && hour < 6) || (hour >= 21 && hour < 24)) {
+              }
+            }
+          } else if (holyStart && day !== 0 && day === startDay) {
+            //
+          } else {
+            // dias normales
+            if (i >= 490) {
+              //horas extra
+              if (hour >= 6 && hour < 21) {
+              }
+              if ((hour >= 0 && hour < 6) || (hour >= 21 && hour < 24)) {
+              }
+            } else {
+              //horas ordinarias
+              if (hour >= 6 && hour < 21) {
+              }
+              if ((hour >= 0 && hour < 6) || (hour >= 21 && hour < 24)) {
+              }
+            }
+          }
+        }
+
+        if (fiveDays) {
+          if (i >= 580) {
+            // horas extra
+            if (hour >= 6 && hour < 21) {
+            }
+            if ((hour >= 0 && hour < 6) || (hour >= 21 && hour < 24)) {
+            }
+          } else {
+            //horas ordinarias
+            if (hour >= 6 && hour < 21) {
+            }
+            if ((hour >= 0 && hour < 6) || (hour >= 21 && hour < 24)) {
+            }
+          }
+        }
+
+        // if ((hour >= 0 && hour < 6) || (hour >= 21 && hour < 24)) {
+        //   if (day === 0) {
+        //     dominicalNigthHours++;
+        //   } else if (holyStart && day !== 0 && day === startDay) {
+        //     holyNigthHours++;
+        //   } else if (holyEnd && day !== 0 && day > startDay) {
+        //     holyNigthHours++;
+        //   } else if (sixDays && i >= 490) {
+        //     hen++;
+        //   } else if (fiveDays && i >= 580) {
+        //     hen++;
+        //   } else {
+        //     nigthHours++;
+        //   }
+        // }
+
+        // if (hour >= 6 && hour < 21) {
+        //   if (day === 0) {
+        //     dominicalOrdinaryHours++;
+        //   } else if (holyStart && day !== 0 && day === startDay) {
+        //     holyOrdinaryHours++;
+        //   } else if (holyEnd && day !== 0 && day > startDay) {
+        //     holyOrdinaryHours++;
+        //   } else if (sixDays && i >= 490) {
+        //     hed++;
+        //   } else if (fiveDays && i >= 580) {
+        //     hed++;
+        //   } else {
+        //     ordinaryHours++;
+        //   }
+        // }
+      }
+
+      let values = {
+        HT: totalHours,
+        HO: (ordinaryHours / 60).toFixed(2),
+        HED: (hed / 60).toFixed(2),
+        HN: (nigthHours / 60).toFixed(2),
+        HEN: (hen / 60).toFixed(2),
+        RN: (nigthHours / 60).toFixed(2),
+        HDD: (dominicalOrdinaryHours / 60).toFixed(2),
+        HND: (dominicalNigthHours / 60).toFixed(2),
+        HDF: (holyOrdinaryHours / 60).toFixed(2),
+        HNF: (holyNigthHours / 60).toFixed(2),
+      };
+      setResults(values);
+      console.log(values);
+    } else {
       return;
     }
-
-    let hoursMillis = endDate.millis - startDate.millis;
-    let totalHours = hoursMillis / 3600000;
-    let ordinaryHours = 0;
-    let nigthHours = 0;
-    let dominicalNigthHours = 0;
-    let dominicalOrdinaryHours = 0;
-    let holyOrdinaryHours = 0;
-    let holyNigthHours = 0;
-
-    let numCycles = hoursMillis / 60000;
-
-    let startDay = new Date(startDate.millis).getDay();
-
-    for (let i = 0; i < numCycles; i++) {
-      let hour = new Date(startDate.millis + 60000 * i).getHours();
-      let day = new Date(startDate.millis + 60000 * i).getDay();
-
-      if ((hour >= 0 && hour < 6) || (hour >= 21 && hour < 24)) {
-        if (day === 0) {
-          dominicalNigthHours++;
-        } else if (holyStart && day !== 0 && day === startDay ) {
-          holyNigthHours++;
-        } else if(holyEnd && day !== 0 && day > startDay){
-          holyNigthHours++;
-        } else {
-          nigthHours++;
-        }
-      }
-      if (hour >= 6 && hour < 21) {
-        if (day === 0) {
-          dominicalOrdinaryHours++;
-        } else if (holyStart && day !== 0 && day === startDay) {
-          holyOrdinaryHours++;
-        } else if(holyEnd && day !== 0 && day > startDay){
-          holyOrdinaryHours++;
-        } else {
-          ordinaryHours++;
-        }
-      }
-    }
-
-    console.log("Horas totales: " + totalHours.toFixed(2));
-    console.log("Horas ordinarias: " + (ordinaryHours / 60).toFixed(2));
-    console.log("Horas Nocturnas: " + (nigthHours / 60).toFixed(2));
-    console.log(
-      "H. Diurnas Dominical: " + (dominicalOrdinaryHours / 60).toFixed(2)
-    );
-    console.log(
-      "H. Nocturnas Dominical: " + (dominicalNigthHours / 60).toFixed(2)
-    );
-    console.log(
-      "H. Diurnas Festivas: " + (holyOrdinaryHours / 60).toFixed(2)
-    );
-    console.log(
-      "H. Nocturnas Festivas: " + (holyNigthHours / 60).toFixed(2)
-    );
-    setResults({
-      HT: totalHours.toFixed(2),
-      HO:(ordinaryHours / 60).toFixed(2),
-      HN:(nigthHours / 60).toFixed(2),
-      HDD:(dominicalOrdinaryHours / 60).toFixed(2),
-      HND:(dominicalNigthHours / 60).toFixed(2),
-      HDF:(holyOrdinaryHours / 60).toFixed(2),
-      HFN:(holyNigthHours / 60).toFixed(2)
-    })
   }
 
   function convertTime(updater, value) {
@@ -194,7 +257,7 @@ function ExtraHours() {
         <NewHour>
           <h2>Nueva horario extra</h2>
           <div>
-            <p>lunes a vierne?</p>
+            <p>jornada de 5 dias?</p>
             <label>
               Si
               <input
@@ -202,13 +265,13 @@ function ExtraHours() {
                 checked={fiveDays}
                 onChange={(e) => {
                   setFiveDays(e.target.checked);
-                  setSixDays(!e.target.checked)
+                  setSixDays(!e.target.checked);
                 }}
               />
             </label>
           </div>
           <div>
-            <p>lunes a sabado?</p>
+            <p>jornada de 6 dias?</p>
             <label>
               Si
               <input
@@ -216,7 +279,7 @@ function ExtraHours() {
                 checked={sixDays}
                 onChange={(e) => {
                   setSixDays(e.target.checked);
-                  setFiveDays(!e.target.checked)
+                  setFiveDays(!e.target.checked);
                 }}
               />
             </label>
@@ -262,7 +325,7 @@ function ExtraHours() {
                 onChange={(e) => {
                   setHolyEnd(e.target.checked);
                 }}
-                disabled={endDate.day===startDate.day?true:false}
+                disabled={endDate.day === startDate.day ? true : false}
               />
             </label>
           </div>
@@ -279,7 +342,13 @@ function ExtraHours() {
           <p>Resultados</p>
           <ul>
             <li>Horas totales: {results.HT}</li>
-            <li>Extras Diurnas(125%):  </li>
+            <li>Horas Ordinarias: {results.HO}</li>
+            <li>Extras Diurnas(125%): {results.HED} </li>
+            <li>Recargo Nocturno(35%): {results.RN}</li>
+            <li>Extras Nocturnas(175%): {results.HEN}</li>
+            <li>Extras Festivas(175%): {results.HDF + results.HNF}</li>
+            <li>Extras Dominical(175%): {results.HDD + results.HND}</li>
+            <li>Recargo Festivo(75%): {results.HNF + results.HND}</li>
           </ul>
         </article>
       </section>
